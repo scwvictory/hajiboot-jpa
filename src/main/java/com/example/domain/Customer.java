d499163b98da3ea80f429df15cf85deab6576ae8
Tetsuya Matsushita <s.c.w-victory.m@gf7.so-net.ne.jp>
2016-09-08 23:17:59 +0900

    リポジトリ新規作成

---
+++ src/main/java/com/example/domain/Customer.java
@@ -0,0 +1,23 @@
+package com.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Customers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	@Column(nullable = false)
+	private String firstName;
+	
+	@Column(nullable = false)
+	private String lastName;
+}
